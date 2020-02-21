03b229438260317800fa7adf78e3a88538aa1762
Kyl2061876116 <2061876116@qq.com>
2020-02-21 13:48:02 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/kyl20200221/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bw.kyl20200221;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //创建的第一个布局
+        //创建分支
+    }
+}
